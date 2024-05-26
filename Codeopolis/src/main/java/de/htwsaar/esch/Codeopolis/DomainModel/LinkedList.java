8f373c948e155a30f04158fe92ac5532128c0645
Christian Petry <christianpetry5@gmail.com>
2024-05-26 23:00:34 +0200

    LinkedList

---
+++ Codeopolis/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/LinkedList.java
@@ -0,0 +1,112 @@
+package de.htwsaar.esch.Codeopolis.DomainModel;
+
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+public class LinkedList<T> {
+
+    private Node head;
+    private int size;
+
+    public class Node {
+        T data;
+        Node next;
+        
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+    
+    public void addLast(T data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public T removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node current = head;
+        head = head.next;
+        size--;
+        return current.data;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public T set (int index, T data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        T oldData = current.data;
+        current.data = data;
+        return oldData;
+    }
+
+    public void clear () {
+        head = null;
+        size = 0;
+    }
+
+    public T remove (int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        Node removeNode = current.next;
+        current.next = removeNode.next;
+        size--;
+        return removeNode.data;
+    }
+
+    public void iterator (Consumer<T> consumer) {
+        Node current = head;
+        while (current != null) {
+            consumer.accept(current.data);
+            current = current.next;
+        }
+    }
+
+}
