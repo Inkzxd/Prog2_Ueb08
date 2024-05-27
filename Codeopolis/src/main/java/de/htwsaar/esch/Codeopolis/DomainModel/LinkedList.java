66bd64c75dc3a8608b81b3a278b5596725b718f5
Christian Petry <christianpetry5@gmail.com>
2024-05-27 13:29:03 +0200

    JavaDoc

--- Codeopolis/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/LinkedList.java
+++ Codeopolis/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/LinkedList.java
@@ -3,11 +3,20 @@ package de.htwsaar.esch.Codeopolis.DomainModel;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
+/**
+ * Klasse LinkedList mit generischem Typ T
+ * Beinhält Methoden zum Hinzufügen, Entfernen, Setzen, Leeren, Größe und Iterieren
+ * Beinhalet eine innere Klasse Node
+ * @author Christian Petry
+ * @version 1.0
+ */
 public class LinkedList<T> {
 
+    // Attribute
     private Node head;
     private int size;
 
+    // Innere Klasse Node
     public class Node {
         T data;
         Node next;
@@ -18,11 +27,16 @@ public class LinkedList<T> {
         }
     }
 
+    // Standardkonstruktor, initialisiert head mit null und size mit 0
     public LinkedList() {
         this.head = null;
         this.size = 0;
     }
     
+    /**
+     * Methode zum Hinzufügen eines Elements am Ende der Liste
+     * @param data Element, das hinzugefügt werden soll
+     */
     public void addLast(T data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -37,6 +51,10 @@ public class LinkedList<T> {
         size++;
     }
 
+    /**
+     * Methode zum Entfernen des Elements am Anfang der Liste
+     * @return data Element, das entfernt wurde
+     */
     public T removeFirst() {
         if (head == null) {
             throw new NoSuchElementException();
@@ -47,14 +65,27 @@ public class LinkedList<T> {
         return current.data;
     }
 
+    /**
+     * Methode zum Überprüfen, ob die Liste leer ist
+     * @return true, wenn die Liste leer ist, sonst false
+     */
     public boolean isEmpty() {
         return head == null;
     }
 
+    /**
+     * Methode zur Ausgabe der Größe der Liste
+     * @return size Größe der Liste
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Methode zur Ausgabe eines Elements an einem bestimmten Index
+     * @param index Index des Elements
+     * @return data Element an dem Index
+     */
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
@@ -66,6 +97,12 @@ public class LinkedList<T> {
         return current.data;
     }
 
+    /**
+     * Methode zum ersetzten eines Elements an einem bestimmten Index
+     * @param index Index des Elements
+     * @param data neues Element
+     * @return oldData altes Element
+     */ 
     public T set (int index, T data) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
@@ -79,11 +116,19 @@ public class LinkedList<T> {
         return oldData;
     }
 
+    /**
+     * Methode zum Leeren der Liste
+     */
     public void clear () {
         head = null;
         size = 0;
     }
 
+    /**
+     * Methode zum Entfernen eines Elements an einem bestimmten Index
+     * @param index Index des Elements
+     * @return removeNode Element, das entfernt wurde
+     */
     public T remove (int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
@@ -101,6 +146,10 @@ public class LinkedList<T> {
         return removeNode.data;
     }
 
+    /**
+     * Methode zum Iterieren über die Liste
+     * @param consumer Consumer, der das Element akzeptiert
+     */
     public void iterator (Consumer<T> consumer) {
         Node current = head;
         while (current != null) {
@@ -109,6 +158,9 @@ public class LinkedList<T> {
         }
     }
 
+    /**
+     * Methode zur Ausgabe der Liste als String
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
