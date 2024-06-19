9dcc340067e6bb06d9f3a977cfb85a6110b3c08b
Christian Petry <christianpetry5@gmail.com>
2024-06-19 14:34:09 +0200

    test

--- Aufgabe_1/InventoryDialog.java
+++ Aufgabe_1/InventoryDialog.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class InventoryDialog {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private void start () {
+        System.out.println("Welcome to the inventory management system!");
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
+    }
+
+    public static void main (String[] args) {
+        new InventoryDialog().start();
+    }
     
 }
